c4e0094e9d1a588fa6643d7ad2f836f36f93413f
yijun <yijun.22@intl.zju.edu.cn>
2023-03-14 04:16:55 +0100

    some simple math

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,1 +1,6 @@
-
+a = -3.19
+b = -118.24
+c = 116.39
+d = a-b
+e = c-a
+print("the longitude distance that Rob travelled to Los Angeles=",d,"the longitude distance that Rob travelled to Haining=",e)
