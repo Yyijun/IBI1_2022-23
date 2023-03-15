41dd246df3b1673bdc21eb98a5fd2d2e2842237f
yijun <yijun.22@intl.zju.edu.cn>
2023-03-15 05:00:48 +0100

    update Breed like rabbits

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -3,15 +3,18 @@ n = 2
 #it is the first generation
 generation = 1
 #stop when having at least 100 rabbits
-if n < 100:
+while 1 == 1:
 #  the number of rabbits that will be born at this gneration
    born_rabbits_number = n
 #  the total number of rabbits
    n = n+born_rabbits_number
 #  count the number of generations
    generation = generation+1
-
+#  if less than 100: keep breeding
+#  if at least 100: DONE
+   if n > 100:
+      break
 #convert a number into a string
 generation_number = str(generation)
 #print the output
-print ("At",generation_number,"th generation, over 100 rabbits have been born")
+print("At",generation_number,"th generation, over 100 rabbits have been born")
