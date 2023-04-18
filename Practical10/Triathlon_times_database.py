ad7001ba5e1b5cc56291cc3f8cacf1d9c78596e5
yijun <yijun.22@intl.zju.edu.cn>
2023-04-18 04:35:46 +0100

    Triathlon times database

---
+++ Practical10/Triathlon_times_database.py
@@ -0,0 +1,17 @@
+#aim: write a new Python class that can be used to contain information about individual athletes and their recorded times for each of the three disciplines (swim, cycle, run)
+#create a class called 'triathlon'
+class triathlon(object):
+   def __init__(self, a, b, c, d, e):
+       #define attributes
+       self.name=a
+       self.location=b
+       self.swim=c
+       self.cycle=d
+       self.run=e
+       self.total=c+d+e
+   def information(self):
+       print("name:",self.name," location:",self.location," swim time:",self.swim, " cycle time:",self.cycle, " run time:", self.run," total time:", self.total)
+
+#an example of how this class can be used
+Enna = triathlon("Enna Green", "China", 3, 4, 5) 
+Enna.information()
