9c4db4cf7afbf6ff03fa25b9f9aecaacc8e0fd7f
Yyijun <127166783+Yyijun@users.noreply.github.com>
2023-05-21 17:03:23 +0800

    Update the comments

--- Practical10/Triathlon_times_database.py
+++ Practical10/Triathlon_times_database.py
@@ -1,5 +1,4 @@
 #aim: write a new Python class that can be used to contain information about individual athletes and their recorded times for each of the three disciplines (swim, cycle, run)
-#please input all the time in second or minute
 #create a class called 'triathlon'
 class triathlon(object):
    def __init__(self, a, b, c, d, e):
@@ -11,8 +10,8 @@ class triathlon(object):
        self.run=e
        self.total=c+d+e
    def information(self):
-       print("name:",self.name," location:",self.location," swim time:",self.swim, " cycle time:",self.cycle, " run time:", self.run," total time:", self.total)
+       print("name:",self.name," location:",self.location," swim time:",self.swim, "s", " cycle time:",self.cycle, "s", " run time:", self.run, "s", " total time:", self.total, "s")
 
-#an example of how this class can be used
-Enna = triathlon("Enna Green", "China", 3, 4, 5) 
+#an example of how this class can be used 
+Enna = triathlon("Enna Green", "China", 3, 4, 5) #please input all the time in second and do not include the unit when inputing the data
 Enna.information()
