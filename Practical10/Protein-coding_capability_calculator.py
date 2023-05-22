42888899d56433891a8810773a24fb55600c0f76
Yyijun <127166783+Yyijun@users.noreply.github.com>
2023-05-22 19:12:43 +0800

    add the comments of the chosen solution

--- Practical10/Protein-coding_capability_calculator.py
+++ Practical10/Protein-coding_capability_calculator.py
@@ -1,12 +1,12 @@
 #aim: write a function that determines whether a given DNA sequence is likely to be protein-coding or not
 #define a function with the name "sequence"
-def sequence(dna_seq):
+def sequence(dna_seq): #when calculating the percentage of the sequence which is coding, this function does not consider the start and stop codons
    #import module
    import re
    dna_seq=dna_seq.upper() #convert all the letters in the dna_seq into capital letters
    within_codons=re.findall(r'ATG(.*)TGA', dna_seq) #read out the bases between 'ATG' and 'TGA'
    within_codons="".join(within_codons) #convert the list into string
-   count=len(within_codons)
+   count=len(within_codons) #count does not consider the start and stop codons
    total_count=len(dna_seq)
    proportion=count/total_count*100
    if proportion > 50:
